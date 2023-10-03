2a15d7ce31fdec9e94f745b6a379c9c47495d75d
Ethan Ellerstein <eellerstein@gmail.com>
2023-10-03 16:17:40 -0500

    updated script to consitently work when ran; expanded meme library

--- script.py
+++ script.py
@@ -1,40 +1,39 @@
 import requests
-from bs4 import BeautifulSoup
 import random
 import webbrowser
 
-# Define the URL of the website where you want to fetch memes
-url = 'https://www.memedroid.com/memes/random'
+# Define the URL of the "imgflip.com" API to fetch memes
+api_url = 'https://api.imgflip.com/get_memes'
 
-# Send an HTTP GET request to the website
-response = requests.get(url)
+# Send an HTTP GET request to the API
+response = requests.get(api_url)
 
 # Check if the request was successful
 if response.status_code == 200:
-    # Parse the HTML content of the page
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Find all image elements on the page (customize this based on the website structure)
-    images = soup.find_all('img')
-
-    # Filter images to select memes based on your criteria
-    memes = [img for img in images if 'meme' in img.get('alt', '').lower()]
+    print("HTTP request successful")
+    
+    # Parse the JSON content of the response
+    meme_data = response.json()
+    
+    # Extract the list of memes from the data
+    memes = meme_data.get('data', {}).get('memes', [])
 
     if memes:
+        print("Memes found")
+
         # Choose a random meme from the list
         random_meme = random.choice(memes)
 
         # Get the meme's image URL
-        meme_url = random_meme['src']
+        meme_url = random_meme['url']
 
         # Display the meme in a web browser
-        webbrowser.register('opera', None, webbrowser.BackgroundBrowser("C:\\Users\\ethan\\AppData\\Local\\Programs\\Opera GX\\launcher.exe"))
-        webbrowser.get('opera').open_new_tab(meme_url)
+        webbrowser.open_new_tab(meme_url)
 
         print(f'Fetched and opened a random meme in your default web browser.')
     else:
-        print('No memes found on the page.')
+        print('No memes found in the API response.')
 else:
-    print('Failed to fetch the website.')
+    print('Failed to fetch memes from the API.')
 
 # Remember to handle exceptions, error handling, and customize for your specific use case.
